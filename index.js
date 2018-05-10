87f50c0c58e778d67d472bdb10824313e5f78b6c
Emily Wessel <ettl02@gmail.com>
2018-05-10 15:01:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ function sayHiToGrandma(string){
     console.log("I can't hear you!")
     return
 }
+function sayHiToGrandma(string) {
+  if ((string)===uppercase) {
+    return 'YES INDEED!'
+  }
+  else if ((string)===lowercase) {
+    return "I can't hear you!"
+  }
+  else if ((string)==="I love you, Grandma.")) {
+    return "I love you, too."
+  }
+  }
+}
