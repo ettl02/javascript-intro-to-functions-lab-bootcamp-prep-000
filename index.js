a17d758efbdf20b206fa89cf8d4fc4cb0fcdfa14
Emily Wessel <ettl02@gmail.com>
2018-05-10 15:04:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ function whisper (string) {
   return string.toLowerCase()
 }
 
+function logWhisper(string) {
+    console.log() string.toLowerCase
+}
 function sayHiToGrandma(string) {
   if ((string)===uppercase) {
     return 'YES INDEED!'
